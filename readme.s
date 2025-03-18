3e76b1e170f363045d146a29e7a9dd9c298f0e35
Muhammet Taha ATAŞ <115085445+tahaatas23@users.noreply.github.com>
2025-03-18 12:37:58 +0300

    Create readme.s

---
+++ readme.s
@@ -0,0 +1,8 @@
+Lab 01: Basic Assembly Commands
+Lab 02: Post Index-Preindex and Conditional Instructions in Assembly
+Lab 03: Stack Operations and Subroutine in Assembly
+Lab 04: Blinking LED on TM4C123GH6PMI Launch Pad using Assembly
+Lab 05: Blinking LED with Buttons on TM4C123GH6PMI Launch Pad using Assembly
+Lab 06: Timer and Interrupts in Assembly
+Lab 07: UART Transmitter and Receiver in Assembly
+Lab 08: ADC in Assembly
